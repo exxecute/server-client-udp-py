f36c87b034c89e03b905a052d7ad5fca02e79c7e
3x3cvte <3xecvte@gmail.com>
2023-09-20 14:22:44 +0300

    added socket api

---
+++ src/udp/socket.py
@@ -0,0 +1,15 @@
+import socket
+
+class SocketAPI():
+    def __init__(self, IP, port):
+        self.create_socket(IP, port)
+
+    def create_socket(self, IP, port):
+        self.server_address_port = (IP, port)
+        self.client_socket = socket.socket(family= socket.AF_INET, type= socket.SOCK_DGRAM)
+
+    def send_to(self, bytes_to_send):
+        self.client_socket.sendto(bytes_to_send, self.server_address_port)
+
+    def recieve_from(self):
+        self.client_socket.recvfrom(1024)
